2720cf08ff14a658e13230526ca85ec8cc34dbec
theo <theoanderson740@gmail.com>
2023-03-16 21:05:46 +0530

    sdf

---
+++ RavenDbFinalTest/Graphql/QueryResolver.cs
@@ -0,0 +1,23 @@
+﻿using Raven.Client.Documents;
+using RavenDbFinalTest.Models;
+namespace RavenDbFinalTest.Graphql
+{
+    public class QueryResolver
+    {
+        private readonly IDocumentStore _documentStore;
+
+        public QueryResolver(IDocumentStore documentStore)
+        {
+            _documentStore = documentStore;
+        }
+
+        public bool EmailExists(string email)
+        {
+            using (var session = _documentStore.OpenSession())
+            {
+                return session.Query<Company>().Any(e => e.EmailId == email);
+            }
+        }
+    }
+
+}
--- RavenDbFinalTest/Program.cs
+++ RavenDbFinalTest/Program.cs
@@ -1,5 +1,7 @@
-
+using Raven.Client.Documents;
 using System.Security.Cryptography.X509Certificates;
+using HotChocolate.AspNetCore;
+using RavenDbFinalTest.Graphql;
 
 
 var certificate = new X509Certificate2("ClientCertificate.pfx", "Theophilus");
@@ -7,12 +9,27 @@ var certificate = new X509Certificate2("ClientCertificate.pfx", "Theophilus");
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .AddGraphQL(sp => SchemaBuilder.New()
+        .AddQueryType<QueryResolver>()
+        .Create());
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromSeconds(500);
 });
+builder.Services.AddSingleton<IDocumentStore>(provider =>
+    new DocumentStore
+    {
+        Urls = new[] { "https://a.theophilus.ravendb.community" },
+        Database = "TestEmployee",
+        Certificate = new X509Certificate2("ClientCertificate.pfx", "Theophilus")
+    }.Initialize());
+builder.Services.AddGraphQLServer()
+        .AddQueryType<QueryResolver>();
+    //    .AddSchemaFromFile("./GraphQL/schema.graphql"); 
+
 
 var app = builder.Build();
 
@@ -26,13 +43,15 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
+
+
+app.MapGraphQL("/graphql");
+
 
 app.UseAuthorization();
 app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
